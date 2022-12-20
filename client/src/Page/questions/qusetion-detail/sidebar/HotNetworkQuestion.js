a2c28a33b17ffbeba43e7946867fdfaf59311cf0
seungheonkim <seungheon818@naver.com>
2022-12-20 16:37:16 +0700

    feat: questionDetail hard-coding component 분리

---
+++ client/src/Page/questions/qusetion-detail/sidebar/HotNetworkQuestion.js
@@ -0,0 +1,15 @@
+const HotNetworkQuestion = () => {
+    return (
+        <div className={'sidebar-hot-network-questions'}>
+            {/*sidebar hot network questions*/}
+            <h4>Related</h4>
+            <div className={'hot-network-questions'}>
+                <div className={'hot-network-questions-container'}>
+                    <span>hot-network-questions!</span>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default HotNetworkQuestion;
