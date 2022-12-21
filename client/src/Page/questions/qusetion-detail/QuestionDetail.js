4b5c229484d46e96e598d940d71c97172e3127c5
seungheonkim <seungheon818@naver.com>
2022-12-21 09:55:23 +0700

    feat: questionDetail working

--- client/src/Page/questions/qusetion-detail/QuestionDetail.js
+++ client/src/Page/questions/qusetion-detail/QuestionDetail.js
@@ -2,15 +2,30 @@ import QuestionHeader from "./QuestionHeader";
 import AskModiView from "./AskModiView";
 import SideBar from "./sidebar/SideBar";
 import MainBar from "./mainbar/MainBar";
+import styled from "styled-components";
+
+const InnerContent = styled.div`
+  max-width: 1100px;
+  width: calc(100% - 164px);
+  background-color: hsl(0, 0%, 100%);
+  border-radius: 0;
+  border: 1px solid hsl(210,8%,85%);;
+  border-top-width: 0;
+  border-bottom-width: 0;
+  border-left-width: 1px;
+  border-right-width: 0;
+  padding: calc(24px * 1);;
+  box-sizing: border-box;
+`;
 
 const questionDetail = (props) => {
     return (
-        <div className={'inner-content'}>
+        <InnerContent>
             <QuestionHeader/>
             <AskModiView/>
             <MainBar/>
             <SideBar/>
-        </div>
+        </InnerContent>
     )
 };
 
