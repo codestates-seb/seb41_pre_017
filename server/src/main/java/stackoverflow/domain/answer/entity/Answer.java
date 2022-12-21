6da7b279913cea7a8a1474f842fffb8055e1dd33
psh1379 <psha1501@gmail.com>
2022-12-21 17:05:17 +0900

    [Feat] answer 엔티티 타임스탬프 추가

--- server/src/main/java/stackoverflow/domain/answer/entity/Answer.java
+++ server/src/main/java/stackoverflow/domain/answer/entity/Answer.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -19,4 +20,8 @@ public class Answer {
 
     @Column(nullable = false, length = 5000)
     private String content;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
