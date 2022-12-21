9cc9914b8659446e9abb2fb7dcb8cc14b3e84d95
psh1379 <110961521+psh1379@users.noreply.github.com>
2022-12-21 13:56:41 +0900

    Delete server/src/main/java/stackoverflow/domain directory

--- server/src/main/java/stackoverflow/domain/answer/repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package stackoverflow.domain.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import stackoverflow.domain.answer.entity.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long>, CrudRepository<Answer, Long> {
-}
