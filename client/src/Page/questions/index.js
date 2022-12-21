b299d3a3168f27b8a67ff98a6d55fe9a1f700a34
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-21 11:28:47 +0900

    import 위치가 잘못된것을 수정했습니다

--- client/src/Page/questions/index.js
+++ client/src/Page/questions/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-import Question from '../component/QuestionsBox';
+import Question from '../component/questionsBox';
 import db from '../../db.json';
-import BlueBtn from '../component/BlueBtn';
+import BlueBtn from '../component/blueBtn';
 import { Sidebar, Container, Main } from '../global/Sidebar';
 
 const StyledHeader = styled.header`
