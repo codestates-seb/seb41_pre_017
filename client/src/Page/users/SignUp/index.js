f7120c63cfed85294d1cdf28471cc1a5ad914207
seungheonkim <seungheon818@naver.com>
2022-12-22 17:28:33 +0700

    feat: login 페이지 및 회원가입 페이지 목업 구현

--- client/src/Page/users/SignUp/index.js
+++ client/src/Page/users/SignUp/index.js
@@ -1,5 +1,51 @@
+import styled from "styled-components";
+import JoinMessageBox from "./JoinMessageBox";
+import SignUpBox from "../../component/SignUpBox";
+
+const Wrapper = styled.div`
+  max-width: 100%;
+  justify-content: center;
+  margin: 0;
+  background-color: hsl(210, 8%, 95%);
+  display: flex;
+  position: relative;
+  width: 100vw;
+  height: 100vw;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 1264px;
+  margin: 0;
+  background-color: transparent;
+  border-left: 0;
+  border-right: 0;
+  box-sizing: border-box;
+  border-radius: 0;
+  //border: 1px solid hsl(210, 8%, 85%);
+  border-top-width: 0;
+  border-bottom-width: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const SignUp = () => {
-    return <div>회원가입 화면</div>;
+    return (
+        <Wrapper>
+            <Content>
+                <Container>
+                    <JoinMessageBox/>
+                    <SignUpBox/>
+                </Container>
+            </Content>
+        </Wrapper>
+    );
 };
 
 export default SignUp;
