5c65754ce3ab4c561a6d672ef899da9fdb9bcc14
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-22 21:14:14 +0900

    페이지네이션 구현

--- client/src/Page/questions/index.js
+++ client/src/Page/questions/index.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import Question from '../component/questionsBox';
-import db from '../../db.json';
 import BlueBtn from '../component/blueBtn';
 import { Sidebar, Container, Main } from '../global/Sidebar';
+import Pagination from './pagination';
 
 const StyledHeader = styled.header`
     display: flex;
@@ -16,9 +15,8 @@ const H1 = styled.h1`
     font-size: 1.5rem;
 `;
 
-const data = db.questions;
-
 const Questions = () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
     return (
         <Container>
             <Sidebar />
@@ -27,12 +25,7 @@ const Questions = () => {
                     <H1>All Questions</H1>
                     <BlueBtn>Ask Question</BlueBtn>
                 </StyledHeader>
-
-                <section>
-                    {data.map((el) => (
-                        <Question key={el.id} data={el} />
-                    ))}
-                </section>
+                <Pagination url={url}></Pagination>
             </Main>
         </Container>
     );
