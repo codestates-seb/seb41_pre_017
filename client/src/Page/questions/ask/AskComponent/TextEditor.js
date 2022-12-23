561c5c1fbb24a4923501444ca9847b1348610e28
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-24 07:15:42 +0900

    텍스트 에디터- 텍스트를 HTML로 변환하는 기능 구현

--- client/src/Page/questions/ask/AskComponent/TextEditor.js
+++ client/src/Page/questions/ask/AskComponent/TextEditor.js
@@ -1,16 +1,17 @@
-import styled from "styled-components";
-import heading from "../../img/textEditor/types.png";
-import bold from "../../img/textEditor/bold-text.png";
-import italic from "../../img/textEditor/italic.png";
-import inlineCode from "../../img/textEditor/html-coding.png";
-import link from "../../img/textEditor/link.png";
-import quote from "../../img/textEditor/quote.png";
-import insertImage from "../../img/textEditor/insert-picture-icon.png";
-import grid from "../../img/textEditor/pixels.png";
-import numlist from "../../img/textEditor/numlist.png";
-import list from "../../img/textEditor/list.png";
-import more from "../../img/textEditor/more.png";
-import help from "../../img/textEditor/help-web-button.png";
+import styled from 'styled-components';
+import heading from '../../img/textEditor/types.png';
+import bold from '../../img/textEditor/bold-text.png';
+import italic from '../../img/textEditor/italic.png';
+import inlineCode from '../../img/textEditor/html-coding.png';
+import link from '../../img/textEditor/link.png';
+import quote from '../../img/textEditor/quote.png';
+import insertImage from '../../img/textEditor/insert-picture-icon.png';
+import grid from '../../img/textEditor/pixels.png';
+import numlist from '../../img/textEditor/numlist.png';
+import list from '../../img/textEditor/list.png';
+import more from '../../img/textEditor/more.png';
+import help from '../../img/textEditor/help-web-button.png';
+import { useState, useRef } from 'react';
 
 // 영역을 두개로 나눈다
 const Wrapper = styled.div`
@@ -18,7 +19,7 @@ const Wrapper = styled.div`
     flex-direction: column;
     margin: 5px 0px 5px 0px;
     border: 1px solid var(--theme-border);
-`
+`;
 
 const Buttons = styled.div`
     margin-top: 6px;
@@ -38,22 +39,34 @@ const Buttons = styled.div`
         }
     }
 
+    .selected {
+        background-color: #c7e0f4;
+        padding-bottom: 23px;
+        padding-top: 5px;
+        border-radius: 5px;
+    }
     button:hover {
         cursor: pointer;
         background-color: #efefef;
     }
-`
+`;
 
 const TextArea = styled.textarea`
-        border-radius: 3px;
-        width: 845px;
-        height: 220px;
-`
+    border-radius: 3px;
+    width: 845px;
+    height: 220px;
+`;
 
+const Preview = styled.div`
+    padding: 10px;
+    line-height: 25px; // 텍스트 줄간격(행간) 조절
+`;
 
-
-const TextEditor = ({data, handleHide, handler}) => {
+const TextEditor = ({ data, handleHide, handler }) => {
     // const imageData = [ heading, bold, italic, inlineCode, link, quote, insertImage, grid, numlist, list, more, help ];
+    const TextAreaFocus = useRef(null); // TextArea포커스 기능을 위한
+    const [selectBtn, isSelectBtn] = useState(); //버튼 선택중
+
     const handleExpect = (e) => {
         // let updatedData = inputData;
         // updatedData[data.id] = e.target.value;
@@ -62,13 +75,59 @@ const TextEditor = ({data, handleHide, handler}) => {
         handler.setExpect(e.target.value);
     };
 
+    // 텍스트를 html 로 바꾸기 위한
+    const [problem, setProblem] = useState('');
+    const [isBold, setBold] = useState(false);
+    const [isItalic, setItalic] = useState(false);
+    const [isInlineCode, setInlineCode] = useState(false);
+
+    // 버튼 이벤트로 해당 html 요소를 넣는 부분
+    const Bold = () => {
+        setBold((current) => !current);
+        if (isBold) {
+            setProblem(problem + '</b>');
+            handler.setProblem(handler.problem + '</b>');
+            isSelectBtn(null);
+        } else {
+            setProblem(problem + '<b>');
+            handler.setProblem(handler.problem + '<b>');
+            isSelectBtn('Bold');
+        }
+        TextAreaFocus.current.focus();
+    };
+    const Italic = () => {
+        setItalic((current) => !current);
+        if (isItalic) {
+            setProblem(problem + '</i>');
+            handler.setProblem(handler.problem + '</i>');
+            isSelectBtn(null);
+        } else {
+            setProblem(problem + '<i>');
+            handler.setProblem(handler.problem + '<i>');
+            isSelectBtn('Italic');
+        }
+        TextAreaFocus.current.focus();
+    };
+    const InlineCode = () => {
+        setInlineCode((current) => !current);
+        if (isInlineCode) {
+            setProblem(problem + '</code>');
+            handler.setProblem(handler.problem + '</code>');
+            isSelectBtn(null);
+        } else {
+            setProblem(problem + '<code>');
+            handler.setProblem(handler.problem + '<code>');
+            isSelectBtn('InlineCode');
+        }
+        TextAreaFocus.current.focus();
+    };
+
+    // 정규식으로 줄바꿈 태그 넣는 부분
     const handleProblem = (e) => {
-        // let updatedData = inputData;
-        // updatedData[data.id] = e.target.value;
-        // setInputData(updatedData);
-        // // console.log(inputData);
+        setProblem(e.target.value.replaceAll(/(\n|\r\n)/g, '<br>'));
         handler.setProblem(e.target.value);
     };
+
     return (
         <Wrapper>
             <Buttons>
@@ -77,28 +136,58 @@ const TextEditor = ({data, handleHide, handler}) => {
                        return <img src={el[idx]} key={idx} alt={el[idx]} ></img>
                     })
                 } */}
-                <button type="button"><img src={heading} alt="heading" /></button>
-                <button><img src={bold} alt="bold" /></button>
-                <button><img src={italic} alt="italic" /></button>
-                <button><img src={inlineCode} alt="inlineCode" /></button>
-                <button><img src={link} alt="link" /></button>
-                <button><img src={quote} alt="quote" /></button>
-                <button><img src={insertImage} alt="insertImage" /></button>
-                <button><img src={grid} alt="grid" /></button>
-                <button><img src={numlist} alt="numlist" /></button>
-                <button><img src={list} alt="list" /></button>
-                <button><img src={more} alt="more" /></button>
-                <button><a href="https://stackoverflow.com/editing-help"> <img src={help} alt="help" /></a></button>
+                <button type="button">
+                    <img src={heading} alt="heading" />
+                </button>
+                {console.log(selectBtn)}
+                <button className={selectBtn === 'Bold' ? 'selected' : null} onClick={Bold} ref={TextAreaFocus}>
+                    <img src={bold} alt="bold" />
+                </button>
+                <button className={selectBtn === 'Italic' ? 'selected' : null} onClick={Italic} ref={TextAreaFocus}>
+                    <img src={italic} alt="italic" />
+                </button>
+                <button className={selectBtn === 'InlineCode' ? 'selected' : null} onClick={InlineCode} ref={TextAreaFocus}>
+                    <img src={inlineCode} alt="inlineCode" />
+                </button>
+                <button>
+                    <img src={link} alt="link" />
+                </button>
+                <button>
+                    <img src={quote} alt="quote" />
+                </button>
+                <button>
+                    <img src={insertImage} alt="insertImage" />
+                </button>
+                <button>
+                    <img src={grid} alt="grid" />
+                </button>
+                <button>
+                    <img src={numlist} alt="numlist" />
+                </button>
+                <button>
+                    <img src={list} alt="list" />
+                </button>
+                <button>
+                    <img src={more} alt="more" />
+                </button>
+                <button>
+                    <a href="https://stackoverflow.com/editing-help">
+                        {' '}
+                        <img src={help} alt="help" />
+                    </a>
+                </button>
             </Buttons>
-            <TextArea 
-                onFocus={handleHide} 
-                onChange={ data.id === 'expect' ? handleExpect : handleProblem} 
-                id={data.id} placeholder={data.placeholder}
-                value={ data.id === 'expect' ? handler.expect : handler.problem }>   
-            </TextArea>
+            <TextArea
+                ref={TextAreaFocus}
+                onFocus={handleHide}
+                onChange={data.id === 'expect' ? handleExpect : handleProblem}
+                id={data.id}
+                placeholder={data.placeholder}
+                value={data.id === 'expect' ? handler.expect : handler.problem}
+            ></TextArea>
+            <Preview dangerouslySetInnerHTML={{ __html: problem }}></Preview> {/* 문자열의 html 태그를 출력 */}
         </Wrapper>
-    )
-}
+    );
+};
 
 export default TextEditor;
-
