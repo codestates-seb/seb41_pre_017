4731f70134f247a5e6776699822cde6242e63946
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-26 01:21:31 +0900

    라우터 경로 추가

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import Footer from './Page/global/Footer';
 import Home from './Page/home';
 import Login from './Page/users/login';
 import SignUp from './Page/users/SignUp';
-import SingleQuestion from './Page/questions/qusetion-detail/index';
+import SingleQuestion from './Page/questions/qusetion-detail';
 import Questions from './Page/questions/questionList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import QuestionAsk from './Page/questions/ask/QuestionAsk';
+import QuestionAsk from './Page/questions/ask';
+import EditPage from './Page/questions/qusetion-detail/editPage';
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
                     <Route path="/questions/ask" element={<QuestionAsk />} />
                     <Route path="/questions" element={<Questions />} />
                     <Route path="/questions/:questionId" element={<SingleQuestion />} />
+                    <Route path="/questions/edit/:category/:id" element={<EditPage />} />
                     <Route path="/users/login" element={<Login />} />
                     <Route path="/users/signUp" element={<SignUp />} />
                 </Routes>
