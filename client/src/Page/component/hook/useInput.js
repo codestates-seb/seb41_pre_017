88d1c051323e2ae9f62a3963f8cd108035de42f5
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-25 19:11:10 +0900

    인풋 커스텀 훅

---
+++ client/src/Page/component/hook/useInput.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+// 1번쨰 인자로 초기값, 2번쨰 인자로 버튼눌렀을때 서버에 전송하는 이벤트 함수
+const useInput = (initialValue = '', onSubmit) => {
+    const [inputValue, setInputValue] = useState(initialValue);
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+        console.log(e.target.value);
+    };
+    const handleSubmit = () => {
+        setInputValue('');
+        onSubmit(inputValue);
+    };
+    return [inputValue, handleChange, handleSubmit];
+};
+
+export default useInput;
