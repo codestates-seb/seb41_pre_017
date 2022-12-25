15303290007b538a4529892d88c4cc3b374575b8
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-25 20:10:27 +0900

    임포트 파일 경로 수정

--- client/src/Page/users/SignUp/SignUpForm.js
+++ client/src/Page/users/SignUp/SignUpForm.js
@@ -1,113 +1,107 @@
-import styled from "styled-components";
-import BlueBtn from "../../component/blueBtn";
-import {Fragment} from "react";
+import styled from 'styled-components';
+import BlueBtn from '../../component/style/blueBtn';
+import { Fragment } from 'react';
 
 const FromContainer = styled.div`
-  max-width: calc(calc(97rem / 12) * 3);
-  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
-  padding: 24px;
-  margin-bottom: 24px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: white;
-  border-radius: 7px;
+    max-width: calc(calc(97rem / 12) * 3);
+    box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
+    padding: 24px;
+    margin-bottom: 24px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    border-radius: 7px;
 `;
 
 const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin-right: 0;
-  margin-left: 0;
-  margin: -6px;
-
-  .auth-item {
     display: flex;
     flex-direction: column;
-    margin: 6px;
     margin-right: 0;
     margin-left: 0;
-  }
+    margin: -6px;
 
-  label {
-    margin: 2px;
-    margin-right: 0;
-    margin-left: 0;
-    cursor: pointer;
-    font-size: 1.15rem;
-    color: hsl(210, 8%, 5%);
-    font-weight: 600;
-    padding: 0 2px;
-  }
+    .auth-item {
+        display: flex;
+        flex-direction: column;
+        margin: 6px;
+        margin-right: 0;
+        margin-left: 0;
+    }
 
-  p {
-    color: hsl(210, 8%, 45%);
-    font-size: 12px;
-    margin-bottom: 4px;
-    margin-top: 4px;
-    clear: both;
-  }
+    label {
+        margin: 2px;
+        margin-right: 0;
+        margin-left: 0;
+        cursor: pointer;
+        font-size: 1.15rem;
+        color: hsl(210, 8%, 5%);
+        font-weight: 600;
+        padding: 0 2px;
+    }
+
+    p {
+        color: hsl(210, 8%, 45%);
+        font-size: 12px;
+        margin-bottom: 4px;
+        margin-top: 4px;
+        clear: both;
+    }
 `;
 
 const InputBox = styled.div`
-  margin-right: 0;
-  margin-left: 0;
-  margin: 2px;
-  position: relative;
-  display: flex;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 2px;
+    position: relative;
+    display: flex;
 
-  input {
-    width: 100%;
-    margin: 0;
-    padding: 0.6em 0.7em;
-    border: 1px solid hsl(210, 8%, 75%);
-    border-radius: 3px;
-    background-color: white;
-    color: hsl(210, 8%, 5%);
-    font-size: 13px;
-  }
+    input {
+        width: 100%;
+        margin: 0;
+        padding: 0.6em 0.7em;
+        border: 1px solid hsl(210, 8%, 75%);
+        border-radius: 3px;
+        background-color: white;
+        color: hsl(210, 8%, 5%);
+        font-size: 13px;
+    }
 `;
 
 const ButtonBox = styled.div`
-  margin-right: 0;
-  margin-left: 0;
-  margin: 6px;
-  display: flex;
-  flex-direction: column;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 6px;
+    display: flex;
+    flex-direction: column;
 
-  .caption {
-    text-align: left;
-    color: hsl(210, 8%, 45%);
-    font-size: 12px;
-    margin-top: 32px;
-  }
+    .caption {
+        text-align: left;
+        color: hsl(210, 8%, 45%);
+        font-size: 12px;
+        margin-top: 32px;
+    }
 
-  .caption a {
-    color: #0074CC;
-  }
+    .caption a {
+        color: #0074cc;
+    }
 `;
 
 const PolicyMessage = (
     <Fragment>
         <span>By clicking “Sign up”, you agree to our </span>
-        <a
-            href="https://stackoverflow.com/legal/terms-of-service/public"
-            name="tos">
+        <a href="https://stackoverflow.com/legal/terms-of-service/public" name="tos">
             terms of service
         </a>
         <span> , </span>
-        <a
-            href="https://stackoverflow.com/legal/privacy-policy"
-            name="privacy">
+        <a href="https://stackoverflow.com/legal/privacy-policy" name="privacy">
             privacy policy
         </a>
         <span> and </span>
-        <a
-            href="https://stackoverflow.com/legal/cookie-policy"
-            name="cookie">
+        <a href="https://stackoverflow.com/legal/cookie-policy" name="cookie">
             cookie policy
         </a>
     </Fragment>
-)
+);
 
 const SignUpForm = () => {
     return (
@@ -116,33 +110,29 @@ const SignUpForm = () => {
                 <div className={'auth-item'}>
                     <label htmlFor={'display-name'}>Display name</label>
                     <InputBox>
-                        <input type={'text'} id={'display-name'} name={'display-name'}/>
+                        <input type={'text'} id={'display-name'} name={'display-name'} />
                     </InputBox>
                 </div>
                 <div className={'auth-item'}>
                     <label htmlFor={'email'}>Email</label>
                     <InputBox>
-                        <input type={'email'} id={'email'} name={'email'}/>
+                        <input type={'email'} id={'email'} name={'email'} />
                     </InputBox>
                 </div>
                 <div className={'auth-item'}>
                     <label htmlFor={'password'}>Password</label>
                     <InputBox>
-                        <input type={'password'} id={'password'} name={'password'}/>
+                        <input type={'password'} id={'password'} name={'password'} />
                     </InputBox>
-                    <p className={'caption'}>
-                        Passwords must contain at least eight characters, including at least 1 letter and 1 number.
-                    </p>
+                    <p className={'caption'}>Passwords must contain at least eight characters, including at least 1 letter and 1 number.</p>
                 </div>
                 <ButtonBox>
                     <BlueBtn>Sign up</BlueBtn>
-                    <div className={'caption'}>
-                        {PolicyMessage}
-                    </div>
+                    <div className={'caption'}>{PolicyMessage}</div>
                 </ButtonBox>
             </Form>
         </FromContainer>
-    )
+    );
 };
 
 export default SignUpForm;
--- client/src/Page/users/login/LogInForm.js
+++ client/src/Page/users/login/LogInForm.js
@@ -1,86 +1,87 @@
-import styled from "styled-components";
-import BlueBtn from "../../component/blueBtn";
+import styled from 'styled-components';
+import BlueBtn from '../../component/style/blueBtn';
 
 const FromContainer = styled.div`
-  max-width: calc(calc(97rem / 12) * 3);
-  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
-  padding: 24px;
-  margin-bottom: 24px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: white;
-  border-radius: 7px;
+    max-width: calc(calc(97rem / 12) * 3);
+    box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
+    padding: 24px;
+    margin-bottom: 24px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    border-radius: 7px;
 `;
 
 const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  margin-right: 0;
-  margin-left: 0;
-  margin: -6px;
-
-  .auth-item {
     display: flex;
     flex-direction: column;
-    margin: 6px;
     margin-right: 0;
     margin-left: 0;
-  }
+    margin: -6px;
 
-  label {
-    margin: 2px;
-    margin-right: 0;
-    margin-left: 0;
-    cursor: pointer;
-    font-size: 1.15rem;
-    color: hsl(210, 8%, 5%);
-    font-weight: 600;
-    padding: 0 2px;
-  }
+    .auth-item {
+        display: flex;
+        flex-direction: column;
+        margin: 6px;
+        margin-right: 0;
+        margin-left: 0;
+    }
+
+    label {
+        margin: 2px;
+        margin-right: 0;
+        margin-left: 0;
+        cursor: pointer;
+        font-size: 1.15rem;
+        color: hsl(210, 8%, 5%);
+        font-weight: 600;
+        padding: 0 2px;
+    }
 `;
 
 const InputContainMessage = styled.div`
-  margin-right: 0;
-  margin-left: 0;
-  margin: 2px;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  
-  span {
-    text-decoration: none;
-    color: hsl(206, 100%, 40%);
-    cursor: pointer;
-    user-select: auto;
-    font-size: 12px;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 2px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    span {
+        text-decoration: none;
+        color: hsl(206, 100%, 40%);
+        cursor: pointer;
+        user-select: auto;
+        font-size: 12px;
+    }
 `;
 
 const InputBox = styled.div`
-  margin-right: 0;
-  margin-left: 0;
-  margin: 2px;
-  position: relative;
-  display: flex;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 2px;
+    position: relative;
+    display: flex;
 
-  input {
-    width: 100%;
-    margin: 0;
-    padding: 0.6em 0.7em;
-    border: 1px solid hsl(210, 8%, 75%);
-    border-radius: 3px;
-    background-color: white;
-    color: hsl(210, 8%, 5%);
-    font-size: 13px;
-  }
+    input {
+        width: 100%;
+        margin: 0;
+        padding: 0.6em 0.7em;
+        border: 1px solid hsl(210, 8%, 75%);
+        border-radius: 3px;
+        background-color: white;
+        color: hsl(210, 8%, 5%);
+        font-size: 13px;
+    }
 `;
 
 const ButtonBox = styled.div`
-  margin-right: 0;
-  margin-left: 0;
-  margin: 6px;
-  display: flex;
-  flex-direction: column;
+    margin-right: 0;
+    margin-left: 0;
+    margin: 6px;
+    display: flex;
+    flex-direction: column;
 `;
 
 const LogInForm = () => {
@@ -90,7 +91,7 @@ const LogInForm = () => {
                 <div className={'auth-item'}>
                     <label htmlFor={'email'}>Email</label>
                     <InputBox>
-                        <input type={'email'} id={'email'} name={'email'}/>
+                        <input type={'email'} id={'email'} name={'email'} />
                     </InputBox>
                 </div>
                 <div className={'auth-item'}>
@@ -99,7 +100,7 @@ const LogInForm = () => {
                         <span>Forgot password?</span>
                     </InputContainMessage>
                     <InputBox>
-                        <input type={'password'} id={'password'} name={'password'}/>
+                        <input type={'password'} id={'password'} name={'password'} />
                     </InputBox>
                 </div>
                 <ButtonBox>
@@ -107,7 +108,7 @@ const LogInForm = () => {
                 </ButtonBox>
             </Form>
         </FromContainer>
-    )
+    );
 };
 
 export default LogInForm;
