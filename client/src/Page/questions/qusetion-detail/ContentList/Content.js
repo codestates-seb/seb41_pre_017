84ee3799fcf0c7a4f89678be86d45f56986bae4e
LeeJoongWon <dlwnddnjs48@gmail.com>
2022-12-28 00:18:27 +0900

    사이드바 디자인 추가

--- client/src/Page/questions/qusetion-detail/ContentList/Content.js
+++ client/src/Page/questions/qusetion-detail/ContentList/Content.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import TimeForToday from '../../../components/function/timeForToday';
 const Post = styled.div`
     line-height: 30px;
     margin: 0px 20px;
@@ -17,6 +17,7 @@ const Container = styled.div`
     display: flex;
     padding-bottom: 50px;
     width: 100%;
+    justify-content: space-between;
 `;
 
 const Tags = styled.nav`
@@ -30,6 +31,7 @@ const UserCard = styled.article`
     flex-direction: row;
     flex-wrap: nowrap;
     justify-content: space-between;
+    margin-left: 10px;
     width: 100%;
     button {
         margin-right: 15px;
@@ -38,24 +40,44 @@ const UserCard = styled.article`
 `;
 const User = styled.div`
     display: flex;
+    align-items: flex-start;
+    flex-direction: column;
     padding: 8px;
     width: 200px;
     border-radius: 5px;
-    background-color: rgb(191, 217, 236);
+    background-color: rgb(213, 229, 241);
+
     div {
         color: rgb(0 116 204);
-        font-size: 13px;
-        margin: 5px;
+        font-size: 10px;
+        margin-left: 5px;
+        margin-bottom: 2px;
     }
     span {
         margin: 5px;
+    }
+    .time {
+        color: rgb(106 115 124);
     }
 `;
 const Button = styled.button`
     cursor: pointer;
 `;
+const UserContainer = styled.div`
+    display: flex;
+`;
+const ProfileImg = styled.img`
+    width: 35px;
+    height: 35px;
+`;
+
+const PostBody = styled.div`
+    flex-grow: 1;
+`;
 
 const Content = ({ category, data, dataHandler, answerData, index }) => {
+    const Modified = TimeForToday(new Date(data.modifiedAt));
+    console.log(Modified);
     // CodeToHtml = 코드화된 데이터 파싱
     const contentData = CodeToHtml(data.content);
     const [feat, setFeat] = useState([Math.floor(Math.random() * 101), Math.floor(Math.random() * 101), Math.floor(Math.random() * 101)]);
@@ -74,7 +96,7 @@ const Content = ({ category, data, dataHandler, answerData, index }) => {
         <Container>
             {/* 투표기능 */}
             <Vote data={category === 'answer' ? answerData : data} index={index} dataHandler={dataHandler}></Vote>
-            <div>
+            <PostBody>
                 {/* 질문&답변  contentData 출력 */}
                 <Post dangerouslySetInnerHTML={{ __html: contentData }} />
                 {/* 질문&답변  태그들 출력 */}
@@ -82,7 +104,7 @@ const Content = ({ category, data, dataHandler, answerData, index }) => {
                 {/* 유저기능 = 질문&답변 수정, 해당유저 정보 */}
                 <UserCard>
                     <ul>
-                        <Button>Share</Button>
+                        {/* <Button>Share</Button> */}
                         <Link
                             to={`/questions/edit/${category}/${data.questionId}`}
                             state={{
@@ -93,20 +115,27 @@ const Content = ({ category, data, dataHandler, answerData, index }) => {
                             <Button>Edit</Button>
                         </Link>
                         <Button onClick={Delete}>Delete</Button>
-                        <Button>Follow</Button>
+                        {/* <Button>Follow</Button> */}
                     </ul>
                     <User>
-                        <img src={ProfilePicture} alt="profile" />
-                        <div>
-                            <div>{data.nickname}</div>
-                            <span>
-                                {/* 업적 = 랜덤함수로 임의의 숫자를 생성 API가 구현된다면 바꿔야됨 */}
-                                {feat[0]}🥇 {feat[1]}🥈 {feat[2]}🥉
-                            </span>
+                        <div className="time">
+                            {category === 'answer' ? 'answered : ' : 'asked : '}
+                            {Modified}
                         </div>
+
+                        <UserContainer>
+                            <ProfileImg src={ProfilePicture} alt="profile" />
+                            <div>
+                                <div>{data.nickname}</div>
+                                <span>
+                                    {/* 업적 = 랜덤함수로 임의의 숫자를 생성 API가 구현된다면 바꿔야됨 */}
+                                    {feat[0]}🥇 {feat[1]}🥈 {feat[2]}🥉
+                                </span>
+                            </div>
+                        </UserContainer>
                     </User>
                 </UserCard>
-            </div>
+            </PostBody>
         </Container>
     );
 };
