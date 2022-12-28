a2a3b5eb80faf145acc7b8928bbbbce6ca1e1380
eunji <omegle3333@gmail.com>
2022-12-28 15:39:48 +0900

    [Fix] App.js 라우터 부분 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Footer from './Page/global/Footer';
 import Home from './Page/home';
 import Login from './Page/users/login';
 import SignUp from './Page/users/SignUp';
-import Profile from './Page/users/profile';
 import SingleQuestion from './Page/questions/qusetion-detail';
 import Questions from './Page/questions/questionList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -23,7 +22,6 @@ function App() {
                     <Route path="/questions/edit/:category/:id" element={<EditPage />} />
                     <Route path="/users/login" element={<Login />} />
                     <Route path="/users/signUp" element={<SignUp />} />
-                    <Route path="/members" element={<Profile />} />
                 </Routes>
                 <Footer />
             </BrowserRouter>
