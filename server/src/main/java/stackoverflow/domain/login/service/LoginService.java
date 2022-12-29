ff9fca3fd011e1220053b6ba214f26228998957b
bangjaeyoung <qkdwo4613@gmail.com>
2022-12-29 17:44:25 +0900

    [Chore] 출력 코드 제거

--- server/src/main/java/stackoverflow/domain/login/service/LoginService.java
+++ server/src/main/java/stackoverflow/domain/login/service/LoginService.java
@@ -15,12 +15,8 @@ public class LoginService {
     }
 
     public Member login(String email, String password) {
-        System.out.println("내가 넣은 이메일 : " + email);
-        System.out.println("내가 넣은 패스워드 : " + password);
-
         Optional<Member> findOptionalMember = memberRepository.findByEmail(email);
         Member member = findOptionalMember.get();
-        System.out.println("member" + member);
 
         if (member.getPwd().equals(password)) {
             return member;
