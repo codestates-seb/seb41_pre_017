5d9cdcb9a7332f8ec69c24f1e63dae84938144d8
eunji <omegle3333@gmail.com>
2022-12-30 22:40:03 +0900

    [Feat] sidebar 수정

---
+++ client/src/Page/questions/qusetion-detail/sidebar/data/relatedData.js
@@ -0,0 +1,39 @@
+const related = [
+    {
+        "voted": 990,
+        "title": "How to solve Plugin execution not covered by lifecycle configuration for Spring Data", 
+        "link": "https://stackoverflow.com/questions/6352208/how-to-solve-plugin-execution-not-covered-by-lifecycle-configuration-for-sprin?rq=1"
+    },
+    {
+        "voted": 23,
+        "title": "Generate classes with jaxb2-maven-plugin from WSDL",        
+        "link": "https://stackoverflow.com/questions/15903249/generate-classes-with-jaxb2-maven-plugin-from-wsdl?rq=1"
+    },
+    {
+        "voted": 2810,
+        "title": "How can I create an executable/runnable JAR with dependencies using Maven?",
+        "link": "https://stackoverflow.com/questions/574594/how-can-i-create-an-executable-runnable-jar-with-dependencies-using-maven?rq=1"
+    },
+    {
+        "voted": 523,
+        "title": "Maven Modules + Building a Single Specific Module",
+        "link": "https://stackoverflow.com/questions/1114026/maven-modules-building-a-single-specific-module?rq=1"
+    },
+    {
+        "voted": 739,
+        "title": "IntelliJ inspection gives Cannot resolve symbol but still compiles code",
+        "link": "https://stackoverflow.com/questions/5905896/intellij-inspection-gives-cannot-resolve-symbol-but-still-compiles-code?rq=1"
+    },
+    {
+        "voted": 10,
+        "title": "Check for life-cycle/phase and file existence in Maven and report error",
+        "link": ""
+    },
+    {
+        "voted": 1810,
+        "title": "Installing specific package version with pip",
+        "link": "https://stackoverflow.com/questions/5226311/installing-specific-package-version-with-pip?rq=1"
+    }
+]
+
+export default related
