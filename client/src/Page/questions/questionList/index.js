61e80c1d2efd034f45e68f4ee621bac6f5ed1800
LeeJoongWon <dlwnddnjs48@gmail.com>
2023-01-02 10:31:39 +0900

    검색데이터에 종속성배열 추가

--- client/src/Page/questions/questionList/index.js
+++ client/src/Page/questions/questionList/index.js
@@ -1,35 +1,35 @@
 import styled from 'styled-components';
 import BlueBtn from '../../components/style/blueBtn';
-import {Container, Main, Sidebar} from '../../global/Sidebar';
+import { Container, Main, Sidebar } from '../../global/Sidebar';
 import Contents from './Contents';
-import {Link, useLocation} from 'react-router-dom';
-import {useState} from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import Loading from '../../components/style/loading';
-import {useGet} from '../../components/hook/API';
-import {useCookies} from 'react-cookie';
+import { useGet } from '../../components/hook/API';
+import { useCookies } from 'react-cookie';
 
 const StyledHeader = styled.header`
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  padding: 30px 20px;
-  border-bottom: 1px solid var(--theme-border);
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    padding: 30px 20px;
+    border-bottom: 1px solid var(--theme-border);
 `;
 
 const H1 = styled.h1`
-  font-size: 1.8rem;
-  font-weight: bold;
+    font-size: 1.8rem;
+    font-weight: bold;
 `;
 
 const Questions = () => {
     const [loading, setLoading] = useState(true);
-    const {state} = useLocation();
-    const [data] = useGet(`questions?page=1&size=200`, setLoading);
+    const { state } = useLocation();
+    const [data] = useGet(`questions?page=1&size=200`, setLoading, state);
     const [cookie] = useCookies(['memberId']);
 
     return (
         <Container>
-            <Sidebar/>
+            <Sidebar />
             <Main>
                 <StyledHeader>
                     <H1>All Questions</H1>
@@ -41,20 +41,19 @@ const Questions = () => {
                         <Link to="../users/login">
                             <BlueBtn>Ask Question</BlueBtn>
                         </Link>
-                    )
-                    }
+                    )}
                 </StyledHeader>
                 {loading ? (
-                    <Loading/>
+                    <Loading />
                 ) : (
                     <Contents
                         _data={
                             state === null
                                 ? data
                                 : data.filter((el) => {
-                                    if (el.title.includes(state)) return el;
-                                    else if (el.nickname.includes(state)) return el;
-                                })
+                                      if (el.title.includes(state)) return el;
+                                      else if (el.nickname.includes(state)) return el;
+                                  })
                         }
                     ></Contents>
                 )}
